670e23982ebee12e16272c98cf38e69b7ef04d82
Gauravv-03 <125569098+Gauravv-03@users.noreply.github.com>
2025-03-05 19:22:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Credit from './components/Credit';
+
 import SearchEngine from "./SearchEngine";
 import Forecast from "./Forecast";
 
